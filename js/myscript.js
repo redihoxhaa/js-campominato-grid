69bc5f80b3df9a05895710e04139caa226c64c47
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-15 22:26:09 +0100

    feat: avanzamento gioco personalizzato

--- js/myscript.js
+++ js/myscript.js
@@ -22,6 +22,17 @@ function generateBoard(elementToAppendTo, loops, difficulty) {
     }
 }
 
+// Funzione per generare bombe
+function generateBombs(boardLength) {
+    const bombsNumber = parseInt(boardLength / 10);
+    const bombsList = [];
+    for (let i = 0; i < bombsNumber; i++) {
+        const bombPosition = Math.floor(Math.random() * boardLength + 1);
+        bombsList.push(bombPosition);
+    }
+    return bombsList;
+}
+
 // OPERATIONS
 
 const board = document.querySelector(".board");
@@ -30,6 +41,7 @@ const loopsArray = [25, 49, 81, 100];
 const chooseDifficulty = document.getElementById("difficulty-selector");
 const playButton = document.getElementById("play-button");
 const callToAction = document.querySelector(".call-to-action");
+let bombsArray = [];
 
 
 // Creazione opzioni in modo dinamico
@@ -48,18 +60,22 @@ playButton.addEventListener("click", function () {
     if (chooseDifficulty.value === gameDifficulty[0]) {
 
         generateBoard(board, loopsArray[0], gameDifficulty[0]);
+        bombsArray = generateBombs(loopsArray[0]);
 
     } else if (chooseDifficulty.value === gameDifficulty[1]) {
 
         generateBoard(board, loopsArray[1], gameDifficulty[1]);
+        bombsArray = generateBombs(loopsArray[1]);
 
     } else if (chooseDifficulty.value === gameDifficulty[2]) {
 
         generateBoard(board, loopsArray[2], gameDifficulty[2]);
+        bombsArray = generateBombs(loopsArray[2]);
 
     } else if (chooseDifficulty.value === gameDifficulty[3]) {
 
         generateBoard(board, loopsArray[3], gameDifficulty[3]);
+        bombsArray = generateBombs(loopsArray[3]);
 
     }
 })
